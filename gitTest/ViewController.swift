96b7a09c7ac7ecdac3b26e978a50f7391ea1def4
HARUKA ito <haruka_ito@keio.jp>
2020-11-15 05:16:43 +0900

    1114

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number: Int = 0
+    @IBOutlet var Label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func p () {
+        number = number + 1
+        Label.text = String(number)
+    }
+    
+    @IBAction func m () {
+        number = number - 1
+        Label.text = String(number)
+    }
+    
+    @IBAction func c () {
+        number = 0
+        Label.text = String(number)
     }
 
 
